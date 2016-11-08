bbe9b852aacf46ff44c98f44598d157eaa83d552
moethu <maximilian.o.e.thumfart@gmail.com>
2016-11-08 13:29:10 +0100

    init

---
+++ DynamoPDF/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Geometry;
+
+namespace DynamoPDF
+{
+    /// <summary>
+    /// Extensions for PDF parsing
+    /// </summary>
+    [IsVisibleInDynamoLibrary(false)]
+    public static class Extensions
+    {
+        /// <summary>
+        /// Parse PDF double value
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="scale"></param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        public static double ToDouble(this PdfObject obj, double scale = 0)
+        { 
+            double value = 0;
+            if (double.TryParse(obj.ToString(), out value))
+                return value *= scale;
+            else
+                return 0;
+        }
+
+        /// <summary>
+        /// Parse PDF Line
+        /// </summary>
+        /// <param name="annotation"></param>
+        /// <param name="scale"></param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        public static Line ToLine(this PdfDictionary annotation, double scale)
+        {
+            PdfArray data = annotation.GetAsArray(PdfName.L);
+            Point start = Point.ByCoordinates(data[0].ToDouble(scale), data[1].ToDouble(scale));
+            Point end = Point.ByCoordinates(data[2].ToDouble(scale), data[3].ToDouble(scale));
+            return Line.ByStartPointEndPoint(start, end);
+        }
+
+        /// <summary>
+        /// Parse PDF Polyline to PolyCurve
+        /// </summary>
+        /// <param name="annotation"></param>
+        /// <param name="scale"></param>
+        /// <returns></returns>
+        [IsVisibleInDynamoLibrary(false)]
+        public static PolyCurve ToPolyCurve(this PdfDictionary annotation, double scale, bool close)
+        {
+            PdfArray data = annotation.GetAsArray(PdfName.VERTICES);
+
+            List<Point> points = new List<Point>();
+            for (int j = 0; j < data.Size - 1; j++)
+            {
+                points.Add(Point.ByCoordinates(data[j].ToDouble(scale), data[j + 1].ToDouble(scale)));
+            }
+
+            return PolyCurve.ByPoints(points, close);
+        }
+    }
+
+}
---
+++ DynamoPDF/Parser.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
+using Dynamo.Utilities;
+using Dynamo.Models;
+
+namespace DynamoPDF
+{
+    /// <summary>
+    /// PDF Parser for Dynamo
+    /// </summary>
+    public static class Parser
+    {
+        /// <summary>
+        /// Read PDF annotations and return their geometries
+        /// </summary>
+        /// <param name="filepath">PDF filepath</param>
+        /// <param name="scale">Conversion factor (scaling)</param>
+        /// <returns>Geometry collection</returns>
+        public static IEnumerable<Geometry> GetGeometries(string filepath, double scale = 0)
+        {
+            if (!System.IO.File.Exists(filepath))
+                throw new Exception(Properties.Resources.FileNotFoundError);
+
+
+            List<Geometry> geometries = new List<Geometry>();
+
+            // Open a new memory stream
+            using (var ms = new System.IO.MemoryStream())
+            {
+                // Create a new pdf reader and get the first page
+                PdfReader myPdfReader = new PdfReader(filepath);
+                PdfDictionary pageDict = myPdfReader.GetPageN(1);
+                PdfArray annotArray = pageDict.GetAsArray(PdfName.ANNOTS);
+
+                // Walk through the annotation element array
+                for (int i = 0; i < annotArray.Size; i++)
+                {
+                    // Get the elements type and subject to filter by
+                    PdfDictionary annotationElement = annotArray.GetAsDict(i);
+                    PdfName subject = annotationElement.GetAsName(PdfName.SUBTYPE);
+                    if (subject != null)
+                    {
+                        if (subject == PdfName.LINE)
+                        {
+                            geometries.Add(annotationElement.ToLine(scale));
+                        }
+                        else if (subject == PdfName.POLYGON)
+                        {
+                            geometries.Add(annotationElement.ToPolyCurve(scale, true));
+                        }
+                        else if (subject == PdfName.POLYLINE)
+                        {
+                            geometries.Add(annotationElement.ToPolyCurve(scale, false));
+                        }
+                    }
+                }
+            }
+
+            return geometries;
+        }
+    }
+
+}
