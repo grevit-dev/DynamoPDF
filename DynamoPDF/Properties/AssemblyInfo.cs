22226e7c12143f61874cb2897e1c1fe5a424dede
moethu <maximilian.o.e.thumfart@gmail.com>
2017-01-28 12:34:28 +0100

    assemblyinfo changed

--- DynamoPDF/Properties/AssemblyInfo.cs
+++ DynamoPDF/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamoPDF")]
-[assembly: AssemblyDescription("Dynamo PDF Importer")]
+[assembly: AssemblyDescription("Dynamo PDF Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DynamoPDF")]
